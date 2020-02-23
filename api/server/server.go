59b0e85d2f5ac6ae08abc1f369022db8d5ff978b
Pavel <p-sawwin@yandex.ru>
2020-02-23 14:18:50 +0000

    add shutdown server

--- api/server/server.go
+++ api/server/server.go
@@ -1,26 +1,31 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"github.com/ebladrocher/smarthome/api/server/controllers"
 	"github.com/ebladrocher/smarthome/system/store"
 	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 // Server ...
 type Server struct {
-	Config *ServerConfig
+	Config      *ServerConfig
 	Controllers *controllers.Controllers
-	server *http.Server
-	router *mux.Router
-	store  store.Store
-	logger *Logger
+	server      *http.Server
+	router      *mux.Router
+	store       store.Store
+	logger      *Logger
+	isStarted   bool
 }
 
 // Server ...
-func (s *Server) Start() {
+func (s *Server) Start() error {
 
 	s.server = &http.Server{
 		Addr:    fmt.Sprintf("%s:%d", s.Config.Host, s.Config.Port),
@@ -32,19 +37,50 @@ func (s *Server) Start() {
 	//	zap.Any(s.Config.Host, s.Config.Port),
 	//)
 
+	go func() {
+		if err := s.server.ListenAndServe(); err != nil {
+			s.logger.Logger.Fatal(
+				"server start error",
+				zap.Error(err),
+			)
+		}
+	}()
 
-	if err := s.server.ListenAndServe(); err != nil {
-		s.logger.Logger.Fatal(
-			"server start error",
-			zap.Error(err),
-		)
-	}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, os.Interrupt)
+
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdown()
+
+	return s.server.Shutdown(ctx)
 }
 
 // Shutdown ...
 
+func (s *Server) Shutdown() {
+
+	if !s.isStarted {
+		return
+	}
+	s.isStarted = false
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := s.server.Shutdown(ctx); err != nil {
+		s.logger.Logger.Fatal(
+			"server running",
+			zap.Error(err),
+		)
+	}
+	s.logger.Logger.Info(
+		"Server exiting",
+	)
+}
+
 // ServeHTTP ...
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
@@ -56,11 +92,11 @@ func NewServer(
 ) (*Server, error) {
 
 	newServer := &Server{
-		Config: cfg,
+		Config:      cfg,
 		Controllers: controllers.NewControllers(),
-		router: mux.NewRouter(),
-		store:  store,
-		logger: log,
+		router:      mux.NewRouter(),
+		store:       store,
+		logger:      log,
 	}
 
 	newServer.setControllers()
